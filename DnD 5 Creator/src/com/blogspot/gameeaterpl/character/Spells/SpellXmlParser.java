e43a02c91bce955ee425577f4c660e6ff5142471
sakurazuka <sakurazuka90@gmail.com>
2015-01-12 23:53:19 +0100

    Dodanie biblioteki SAX i parsera spelli z xml

---
+++ DnD 5 Creator/src/com/blogspot/gameeaterpl/character/Spells/SpellXmlParser.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.blogspot.gameeaterpl.character.Spells;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ *
+ * @author sakurazuka
+ */
+public class SpellXmlParser extends DefaultHandler {
+    
+}
